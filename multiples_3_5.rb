6b3527d823f4d18f5658b2e882b097804538a379
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-06 13:53:40 -0500

    Add a 3rd solution with ruby

--- multiples_3_5.rb
+++ multiples_3_5.rb
@@ -16,3 +16,21 @@ puts sum
 
 (1..999).select { |num| (num % 3 == 0) || (num % 5 == 0) }.reduce(:+)
 
+# OR
+
+numbers = 1..1000
+euler_multiples = []
+numbers.map do |x|
+	if x % 3 == 0 || x % 5 == 0
+		euler_multiples << x
+	end
+end
+
+sum = 0
+
+euler_multiples.each do |y|
+	sum += y
+end
+
+puts sum
+
